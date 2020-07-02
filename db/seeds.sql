1009570db5c89324e64a739ef1a0ce87a0a2414a
Brice Boutet <boutetlb@gmail.com>
2020-07-02 01:15:31 -0400

    Repos created and mvc paradigm template copied

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Chicken Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Beef Double Cheese');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Happy Meal', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big King', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teriyaki', true);
+INSERT INTO burgers (burger_name) VALUES ('Angus');
