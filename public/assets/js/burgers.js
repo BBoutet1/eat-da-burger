7b57734139056588b5b071f9b2900bbc4db3bbcc
Brice Boutet <boutetlb@gmail.com>
2020-07-03 00:37:09 -0400

    Devoured onclick update working

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,25 +1,5 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    // $(".change-devour").on("click", function(event) {
-    //     var id = $(this).data("id");
-    //     var newDevour = $(this).data("newdevour");
-
-    //     var newDevourState = {
-    //         devoured: newDevour
-    //     };
-
-    //     // Send the PUT request.
-    //     $.ajax("/api/burgers/" + id, {
-    //         type: "PUT",
-    //         data: newDevourState
-    //     }).then(
-    //         function() {
-    //             console.log("changed devoured to", newDevour);
-    //             // Reload the page to get the updated list
-    //             location.reload();
-    //         }
-    //     );
-    // });
 
     $(".myBurger").on("submit", function(event) {
         // Make sure to preventDefault on a submit event.
@@ -37,6 +17,26 @@ $(function() {
         }).then(
             function() {
                 console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".devour").on("click", function(event) {
+        var id = $(this).data("id");
+
+        var newDevourState = {
+            devoured: 1
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function() {
+                console.log("Burger devoured");
                 // Reload the page to get the updated list
                 location.reload();
             }
